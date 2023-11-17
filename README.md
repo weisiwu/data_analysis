d24c0420b7f4052cf905ede3f1ab0ad576c43a40
weisiwu <siwu.wsw@autonavi.com>
2023-11-17 14:50:35 +0800

    chore: 添加对阿Bup分析

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # data_analysis
 data_analysis 
+
+1、分析下面的B站up主
+https://space.bilibili.com/675816970
+接口文档依赖
+https://github.com/SocialSisterYi/bilibili-API-collect
---
+++ bilibili_up_675816970.py
@@ -0,0 +1,12 @@
+# https://space.bilibili.com/675816970
+
+# 用户空间基本信息
+# https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/user/info.md
+
+# 认证方式和地下的wbi鉴权方式是一样的吗
+
+# wbi鉴权 - 同目录下还有其他的鉴权方式
+# https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/sign/wbi.md
+
+# 用户状态数 获取粉丝数量、视频播放数、
+# https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/user/status_number.md
